d3adeb2276a48d5ceb4a50c0ac3608ad254e8c82
Metalgurke <klosterhalfen@gmail.com>
2019-04-25 10:48:01 +0200

    Ver.: 0.1 | Methode get_polar hinzugefügt

--- complexutils.py
+++ complexutils.py
@@ -1,21 +1,40 @@
 import math
 
 
-# Funktion git den Betrag einer komplexen Zahl zurueck
+# Funktion gibt den Betrag einer komplexen Zahl zurueck
 def get_complex_magnitude(real, imag):
     return math.sqrt((real * real) + (imag + imag))
 
 
-# Funktion git eine komplexe Zahl in kartesischen Koordinaten zurueck
+# Funktion gibt eine komplexe Zahl in kartesischen Koordinaten zurueck
 def get_cartesian(real, imag):
     return real + 1j * imag
     
 
-# Funktion git eine komplexe Zahl in Polarkoordinaten zurueck
+# Funktion gibt eine komplexe Zahl in Polarkoordinaten zurueck
 def get_polar(real, imag):
-    return 1
-    
+    # Bestimme Betrag der komplexen Zahl / Vektors
+    r = get_complex_magnitude(real, imag)
 
-# Funktion git eine komplexe Zahl in euler'scher Darstellung zurueck
+    # Bestimme Winkel phi in Bogenmaß anhand des Quadraten
+    if real < 0 and imag < 0
+        phi = atan(imag / real) - math.pi
+
+    elif real == 0 and imag < 0
+        phi = -math.pi / 2
+
+    elif real > 0
+        phi = atan(imag / real)
+
+    elif real == 0 and imag > 0
+        phi = math.pi / 2
+
+    elif real < 0 and imag >= 0
+        phi = atan(imag / real) + math.pi
+
+    return { "magnitude" : r, "angle" : phi, "angleGrad" : (phi*180/math.pi)}
+
+
+# Funktion gibt eine komplexe Zahl in euler'scher Darstellung zurueck
 def get_euler(real, imag):
     return 1
