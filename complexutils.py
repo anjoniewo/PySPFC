d0a478a9e77dd8be0c4aa4b2012a36ba42c092d7
Metalgurke <klosterhalfen@gmail.com>
2019-04-25 11:13:25 +0200

    Ver.:0.2|Methode get_polar getestet und Fehler in Methode get_complex_magnitude behoben

--- complexutils.py
+++ complexutils.py
@@ -3,7 +3,7 @@ import math
 
 # Funktion gibt den Betrag einer komplexen Zahl zurueck
 def get_complex_magnitude(real, imag):
-    return math.sqrt((real * real) + (imag + imag))
+    return math.sqrt((real ** 2) + (imag ** 2))
 
 
 # Funktion gibt eine komplexe Zahl in kartesischen Koordinaten zurueck
@@ -17,20 +17,20 @@ def get_polar(real, imag):
     r = get_complex_magnitude(real, imag)
 
     # Bestimme Winkel phi in Bogenmaß anhand des Quadraten
-    if real < 0 and imag < 0
-        phi = atan(imag / real) - math.pi
+    if real < 0 and imag < 0:
+        phi = math.atan(imag / real) - math.pi
 
-    elif real == 0 and imag < 0
+    elif real == 0 and imag < 0:
         phi = -math.pi / 2
 
-    elif real > 0
-        phi = atan(imag / real)
+    elif real > 0:
+        phi = math.atan(imag / real)
 
-    elif real == 0 and imag > 0
+    elif real == 0 and imag > 0:
         phi = math.pi / 2
 
-    elif real < 0 and imag >= 0
-        phi = atan(imag / real) + math.pi
+    elif real < 0 and imag >= 0:
+        phi = math.atan( imag / real ) + math.pi
 
     return { "magnitude" : r, "angle" : phi, "angleGrad" : (phi*180/math.pi)}
 
