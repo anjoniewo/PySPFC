bd752c154d120bbffdf3abb454792b4123cd18d3
Anjo Niewöhner <anjo1291@gmail.com>
2019-04-30 13:57:13 +0200

    V0.6 | GridLines werden eingelesen und in einer Lister gespeichert

--- gridlineparser.py
+++ gridlineparser.py
@@ -1,10 +1,11 @@
 from gridelementparser import GridElementParser
+from gridline import GridLine
 
 
 # Parser-Klasse zum Einleser der Leitungsdaten
 class GridLineParser(GridElementParser):
 	
-	def __init__(self, file_path):
+	def __init__(self, file_path, frequency):
 		
 		super(GridLineParser, self).__init__()
 		
@@ -17,9 +18,42 @@ class GridLineParser(GridElementParser):
 		file_path =  root + str(file_path) + str(".csv")
 		
 		self.read_line_parameters(file_path)
+		self.get_lines_from_csv_dictionary(frequency)
 		
 	def read_line_parameters(self, file_path):
 		self.read_csv_to_dictionary(file_path)
+	
+	def get_lines_from_csv_dictionary(self, frequency):
+		
+		parameter_list = []
+		gridline_list = []
+		
+		list_of_keys = list(self.csv_dictionary.keys())
+		
+		# wenn das csv dictionary nicht leer ist
+		if list_of_keys:
+			number_of_gridnodes = len(self.csv_dictionary[list_of_keys[0]])
+			
+			# alle Eintraege des dictionaries durchgehen
+			for i in range(0, number_of_gridnodes):
+				for key in self.csv_dictionary:
+					if key == "node_i":
+						node_i = self.csv_dictionary[key][i]
+					elif key == "node_j":
+						node_j = self.csv_dictionary[key][i]
+					elif key == "length in km":
+						parameter_list.append(float(self.csv_dictionary[key][i]))
+					elif key == "resistance in Ohm/km":
+						parameter_list.append(float(self.csv_dictionary[key][i]))
+					elif key == "inductive_reactance in mH/km":
+						parameter_list.append(float(self.csv_dictionary[key][i]))
+					elif key == "transverse_resistance in  Ohm/km":
+						parameter_list.append(float(self.csv_dictionary[key][i]))
+					elif key == "capacative_reactance in  nF/km":
+						parameter_list.append(float(self.csv_dictionary[key][i]))
+				
+				gridline = GridLine(frequency, node_i, node_j, parameter_list)
+				gridline_list.append(gridline)
 		
 
-lineparser = GridLineParser("lines")
+lineparser = GridLineParser("lines", 50)
--- gridnodeparser.py
+++ gridnodeparser.py
@@ -1,4 +1,5 @@
 from gridelementparser import GridElementParser
+from gridnode import GridNode
 
 
 # Parser-Klasse zum Einleser der Leitungsdaten
@@ -14,10 +15,50 @@ class GridNodeParser(GridElementParser):
 			file_path) + str(".csv")
 		
 		self.read_node_parameters(file_path)
+		self.get_nodes_from_csv_dictionary()
 	
 	def read_node_parameters(self, file_path):
 		self.read_csv_to_dictionary(file_path)
+		
+	def get_nodes_from_csv_dictionary(self):
+		
+		parameter_list = []
+		gridnode_list = []
+		
+		list_of_keys = list(self.csv_dictionary.keys())
+		
+		# wenn das csv dictionary nicht leer ist
+		if list_of_keys:
+			number_of_gridnodes = len(self.csv_dictionary[list_of_keys[0]])
+			
+			# alle Eintraege des dictionaries durchgehen
+			for i in range(0, number_of_gridnodes):
+				for key in self.csv_dictionary:
+					if key == "name":
+						gridnode_name = self.csv_dictionary[key][i]
+					elif key == "typenumber":
+						type_number = self.csv_dictionary[key][i]
+					elif key == "active_load_power":
+						parameter_list.append(self.csv_dictionary[key][i])
+					elif key == "reactive_load_power":
+						parameter_list.append(self.csv_dictionary[key][i])
+					elif key == "active_injection_power":
+						parameter_list.append(self.csv_dictionary[key][i])
+					elif key == "reactive_injection_power":
+						parameter_list.append(self.csv_dictionary[key][i])
+					elif key == "theta":
+						parameter_list.append(self.csv_dictionary[key][i])
+					elif key == "node_voltage":
+						parameter_list.append(self.csv_dictionary[key][i])
+				
+				gridnode = GridNode(gridnode_name, type_number, parameter_list)
+				gridnode_list.append(gridnode)
+		
+		s = ""
+		
+		
+				
+		
 	
 	
 nodeparser = GridNodeParser("gridnodes")
-s = ""
