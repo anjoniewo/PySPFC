1f85c1c4d4cb11455d5948a3af2ffc5633741722
anjo1291 <anjo1291@gmail.com>
2019-05-04 14:57:37 +0200

    V0.9 | Datenkapselung der Klasse BusAdmittanceMatrix

--- busadmittancematrix.py
+++ busadmittancematrix.py
@@ -11,7 +11,7 @@ class BusAdmittanceMatrix:
         self.matrix = None
 
         # Gridlineliste (Leitungsliste)
-        self.grid_line_list = ()
+        self.__grid_line_list = ()
 
     # Methode zur Aufsummierung aller Admittanzen an einem Knoten
     # Parameter:
@@ -19,12 +19,12 @@ class BusAdmittanceMatrix:
     # [Optional] Modus: True|False (all_admittances), Wenn True: werden alle Laengs- und Queradmittanzen aufsummiert. Wenn False: werden nur die Längsadmittanzen aufsummiert (Queradmittanzen werden igoniert)
     # Rückgabewert: kummulierter Admittanzenwert
     # Quelle: E. Handschin, Elektrische Energieübertragungssysteme. Teil 1: Stationaerer Betriebszustand. Heidelberg: Hueting, 1983, Seite 51
-    def get_sum_of_grid_lines_on_node(self, node_name, get_all_admittances_on_node=True):
+    def __get_sum_of_grid_lines_on_node(self, node_name, get_all_admittances_on_node=True):
 
         # Summenadmittanz an Knoten i und entspricht einem Element der Knotenadmittanzmatrix (KAM)
         sum_admittance = Admittance()
 
-        for grid_line in self.grid_line_list:
+        for grid_line in self.__grid_line_list:
 
             if node_name == grid_line.get_node_name_i() or node_name == grid_line.get_node_name_j():
 
@@ -43,24 +43,24 @@ class BusAdmittanceMatrix:
     def calc_matrix(self, grid_node_list, grid_line_list):
 
         # Setzen der Addmitanzliste
-        self.grid_line_list = grid_line_list
+        self.__grid_line_list = grid_line_list
 
         # quadratische Matrixdimension: nxn
         n = len(grid_node_list)
 
         # Erstellung eines nxn-dimensionalen Numpy-Arrays
-        self.matrix = np.ndarray(shape=(n,n), dtype=object)
+        self.matrix = np.ndarray(shape=(n, n), dtype=object)
 
         for i in range(0, n):
             for j in range(i, n):
 
                 # Diagonalelemente der Matrix i=j
-                if(i == j):
-                    self.matrix[i][j] = self.get_sum_of_grid_lines_on_node(grid_node_list[i].get_name())
+                if i == j:
+                    self.matrix[i][j] = self.__get_sum_of_grid_lines_on_node(grid_node_list[i].get_name())
 
                 # Nicht-Diagonalelemente der Matrix i≠j
                 else:
-                    admittance_elem = self.get_sum_of_grid_lines_on_node(grid_node_list[i].get_name(), False)
+                    admittance_elem = self.__get_sum_of_grid_lines_on_node(grid_node_list[i].get_name(), False)
 
                     if admittance_elem.get_g() is not None:
                         admittance_elem.set_g(admittance_elem.get_g() * -1)
--- grid.py
+++ grid.py
@@ -8,20 +8,20 @@ from busadmittancematrix import BusAdmittanceMatrix
 class Grid:
 
     # Initialisierungskonstruktor
-    def __init__(self, frequency=50):
+    def __init__(self, frequency=50, grid_node_list=list(), grid_line_list=list()):
         
         # Netzfrequenz (default 50 Hz)
-        self.frequency = frequency
+        self.__frequency = frequency
         
         # Liste von Knoten und Liste von Leitungen
-        self.grid_node_list = list()
-        self.grid_line_list = list()
+        self.__grid_node_list = grid_node_list
+        self.__grid_line_list = grid_line_list
 
         # Instanzierung der LoadFlowReporter-Klasse
         self.load_flow_reporter = LoadFlowReporter()
 
         # Instanzierung der BusAdmittanceMatrix-Klasse
-        self.bus_admittanz_matrix = BusAdmittanceMatrix()
+        self.__bus_admittanz_matrix = BusAdmittanceMatrix()
 
     # Methode erstellt einen neuen Netzknoten und fuegt diesen der Knotenliste hinzu
     def create_grid_node(self, name, type, node_parameters):
@@ -31,48 +31,48 @@ class Grid:
 
     # Methode fuegt der Netzknotenliste einen Knoten hinzu
     def add_grid_node(self, grid_node):
-        self.grid_node_list.append(grid_node)
+        self.__grid_node_list.append(grid_node)
 
     # Methode erstellt einen neuen Netzzweig und fuegt diese der Leitungsliste hinzu
     def create_grid_line(self, node_i, node_j, line_parameters):
         # Instanzierung eines neuen GridNode Objektes
-        line = GridLine(self.frequency, node_i, node_j, line_parameters)
+        line = GridLine(self.__frequency, node_i, node_j, line_parameters)
         self.add_grid_line(line)
 
     # Methode fuegt der Leitungsliste einen Netzzweig hinzu
     def add_grid_line(self, grid_line):
-        self.grid_line_list.append(grid_line)
+        self.__grid_line_list.append(grid_line)
 
     # Gibt alle Knoten des Netzes in der Konsole aus
     def print_grid_node_list(self):
-        if not len(self.grid_node_list):
+        if not len(self.__grid_node_list):
             print("\nKeine Knoten in Liste")
         else:
-            for i in range(0, len(self.grid_node_list)):
-                self.grid_node_list[i].info()
+            for i in range(0, len(self.__grid_node_list)):
+                self.__grid_node_list[i].info()
 
         # Gibt alle Knoten des Netzes in der Konsole aus
 
     def print_grid_line_list(self):
-        if not len(self.grid_line_list):
+        if not len(self.__grid_line_list):
             print("\nKeine Leitungen in Liste")
         else:
-            for i in range(0, len(self.grid_line_list)):
-                self.grid_line_list[i].info()
+            for i in range(0, len(self.__grid_line_list)):
+                self.__grid_line_list[i].info()
 
     # Methode zur Berechnung der aktuellen Knotenadmittanz-Matrix
     # Übergabeparameter: Knotenliste [grid_node_list], Admittanzenliste (Gridline-Liste) [grid_line_list]
     def calc_bus_admittance_matrix(self):
-        self.bus_admittanz_matrix.calc_matrix(self.grid_node_list, self.grid_line_list)
+        self.__bus_admittanz_matrix.calc_matrix(self.__grid_node_list, self.__grid_line_list)
 
     # Methode gibt die aktuelle Knotenadmittanzmatrix zurück
     def get_bus_admittanz_matrix(self):
-        return self.bus_admittanz_matrix.matrix
+        return self.__bus_admittanz_matrix.matrix
 
     # Methode gibt die aktuelle Knotenadmittanzmatrix zurück
     def print_bus_admittanz_matrix(self):
         result = ""
-        matrix = self.bus_admittanz_matrix.matrix
+        matrix = self.__bus_admittanz_matrix.matrix
         n = len(matrix)
         for i in range(0, n):
             for j in range(0, n):
