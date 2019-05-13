6facfbd88d249e63579d46e338c98f75647848e5
Anjo Niewöhner <anjo1291@gmail.com>
2019-05-13 09:54:53 +0200

    in_serie => in_series

--- LoadFlowTool/loadflowtool/grid/impedance.py
+++ LoadFlowTool/loadflowtool/grid/impedance.py
@@ -2,43 +2,43 @@ from LoadFlowTool.loadflowtool.utils.complexutils import *
 
 
 class Impedance:
-    # Parameter: r -> Realteil, x -> Imaginärteil, in_serie -> sind Real- und Imaginärteil in serie oder parallel [default: in serie]
-    def __init__(self, r, x, in_serie=True):
-
-        self.__r = None
-        self.__x = None
-
-        if in_serie:
-            # Realteil der Impedanz
-            self.__r = r
-            # Imaginaerteil der Impedanz
-            self.__x = x
-        else:
-            if not r and x:
-                self.__x = x
-
-            if r and not x:
-                self.__r = r
-
-            if r and x:
-                # Realteil der Impedanz
-                self.__r = (r * (x ** 2)) / (r ** 2 + x ** 2)
-                # Imaginaerteil der Impedanz
-                self.__x = ((r ** 2) * x) / (r ** 2 + x ** 2)
-
-    # getter-Methoden
-    def get_real_part(self):
-        return self.__r
-
-    def get_imaginary_part(self):
-        return self.__x
-
-    # setter-Methoden
-    def set_real_part(self, r):
-        self.__r = r
-
-    def set_imaginary_part(self, x):
-        self.__x = x
-
-    def get_magnitude(self):
-        return get_complex_magnitude(self.__r, self.__x)
+	# Parameter: r -> Realteil, x -> Imaginärteil, in_serie -> sind Real- und Imaginärteil in serie oder parallel [default: in serie]
+	def __init__(self, r, x, in_series=True):
+		
+		self.__r = None
+		self.__x = None
+		
+		if in_series:
+			# Realteil der Impedanz
+			self.__r = r
+			# Imaginaerteil der Impedanz
+			self.__x = x
+		else:
+			if not r and x:
+				self.__x = x
+			
+			if r and not x:
+				self.__r = r
+			
+			if r and x:
+				# Realteil der Impedanz
+				self.__r = (r * (x ** 2)) / (r ** 2 + x ** 2)
+				# Imaginaerteil der Impedanz
+				self.__x = ((r ** 2) * x) / (r ** 2 + x ** 2)
+	
+	# getter-Methoden
+	def get_real_part(self):
+		return self.__r
+	
+	def get_imaginary_part(self):
+		return self.__x
+	
+	# setter-Methoden
+	def set_real_part(self, r):
+		self.__r = r
+	
+	def set_imaginary_part(self, x):
+		self.__x = x
+	
+	def get_magnitude(self):
+		return get_complex_magnitude(self.__r, self.__x)
