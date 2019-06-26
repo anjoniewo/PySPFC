ff79914e4e544c30e41e24448d7b967a2637fd8f
anjo1291 <anjo1291@gmail.com>
2019-06-26 13:58:23 +0200

    GridParser properties hinzugefuegt um Code schlanker zu machen.

--- simpleloadflow/loadflowtool/griddataimport/gridparser.py
+++ simpleloadflow/loadflowtool/griddataimport/gridparser.py
@@ -6,6 +6,19 @@ from simpleloadflow.loadflowtool.griddataimport.transformerparser import Transfo
 class GridParser:
 
     def __init__(self, frequency, gridline_file_path="", gridnode_file_path="", transformer_path=""):
-        self.grid_line_parser = GridLineParser(gridline_file_path, frequency)
-        self.grid_node_parser = GridNodeParser(gridnode_file_path)
-        self.transformer_parser = TransformerParser(transformer_path)
+        self.__grid_line_parser = GridLineParser(gridline_file_path, frequency)
+        self.__grid_node_parser = GridNodeParser(gridnode_file_path)
+        self.__transformer_parser = TransformerParser(transformer_path)
+
+    def __get_gridlines(self):
+        return self.__grid_line_parser.get_gridlines()
+
+    def __get_gridnodes(self):
+        return self.__grid_node_parser.get_gridnodes()
+
+    def __get_transformers(self):
+        return self.__transformer_parser.get_transformers()
+
+    gridlines = property(__get_gridlines)
+    gridnodes = property(__get_gridnodes)
+    transformers = property(__get_transformers)
