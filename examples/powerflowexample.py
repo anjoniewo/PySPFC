3de5fc736443244194ae7ab4f1a39b33110de3f9
Metalgurke <klosterhalfen@gmail.com>
2019-09-21 16:34:46 +0200

    update of comments in examples/powerflowexample.py

--- examples/powerflowexample.py
+++ examples/powerflowexample.py
@@ -3,7 +3,9 @@ import pyspfc
 # Grid - object instantiation
 grid = pyspfc.Grid()
 
-# import of grid/network data from 'csv_import' directory in the project folder
+# import of grid/network data
+# if import_path = '' the default directory 'csv_import' in the project folder will be set
+# else the chosen directory will be set
 import_path = ''
 grid.import_csv_data(import_path)
 
@@ -11,7 +13,9 @@ grid.import_csv_data(import_path)
 grid.do_powerflow()
 
 # export of powerflow calculation results
+# results will automatically be exported to an 'export' folder int the import directory
 grid.export_powerflow_results()
 
 # creation of a PDF - report with significant data
+# a pdf file with significant results will automatically be exported to an 'export' folder int the import directory
 grid.create_pdf_report()
